cc7d5e391841912f269ab9c855b539eef205f7e3
r-scott25 <rachelbernal2@gmail.com>
2022-12-07 12:45:06 -0600

    debugging

--- src/Main.java
+++ src/Main.java
@@ -74,31 +74,62 @@ public class Main {
         //////////////////////////**** Challenge *****///////////////////////
         ///////////////////////     Multiple Choice     /////////////////////
 
-        String question = "What is my favorite color?";
-        String choiceOne = "red";
-        String choiceTwo = "green";
-        String choiceThree = "blue";
+//        String question = "What is my favorite color?";
+//        String choiceOne = "red";
+//        String choiceTwo = "green";
+//        String choiceThree = "blue";
+//
+//        String correctAnswer = choiceTwo;
+////        Write a print statement asking the question
+//        System.out.println(question);
+//
+////        Write a print statement giving the answer choices
+//        System.out.println("Choose one: " + choiceOne + "  " + choiceTwo + "  " + choiceThree);
+//
+////        Have the user input an answer
+//        Scanner input = new Scanner(System.in);
+//
+////        Retrive the user's input
+//        String userAnswer = input.next();
+//
+////        If the user's input matches the correct answer, print a congrats message
+//        if (userAnswer.toLowerCase().equals(correctAnswer)) {
+//            System.out.println("Congratulations! You answered correctly!");
+//
+////         If the user's input does not match the correct answer, print an "incorrect" message
+//        } else {
+//            System.out.println("Sorry! That is incorrect. The correct answer is " + correctAnswer + ".");
+//        }
 
-        String correctAnswer = choiceTwo;
-//        Write a print statement asking the question
-        System.out.println(question);
 
-//        Write a print statement giving the answer choices
-        System.out.println("Choose one: " + choiceOne + "  " + choiceTwo + "  " + choiceThree);
+        //////////////////////////**** Challenge *****///////////////////////
+        /////////////////////////     Debugging     ////////////////////////
 
-//        Have the user input an answer
-        Scanner input = new Scanner(System.in);
+//        System.out.println("Let's calculate the area of a triangle");
+//
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Please input the base of the triangle (in inches).");
+//        double base = input.nextDouble();
+//
+//        while (base <= 0) {
+//            System.out.println("That's invalid. Please input the base of the triangle (in inches).");
+//            base = input.nextDouble();
+//        }
+//
+//        System.out.println("Please input the height of the triangle (in inches).");
+//        double height = input.nextDouble();
+//        while (height <= 0) {
+//            System.out.println("That's invalid. Please input the height of the triangle (in inches).");
+//            height = input.nextDouble();
+//        }
+//
+//        double area = (base * height) / 2;
+//        System.out.println("The area is " + area);
 
-//        Retrive the user's input
-        String userAnswer = input.next();
 
-//        If the user's input matches the correct answer, print a congrats message
-        if (userAnswer.toLowerCase().equals(correctAnswer)) {
-            System.out.println("Congratulations! You answered correctly!");
 
-//         If the user's input does not match the correct answer, print an "incorrect" message
-        } else {
-            System.out.println("Sorry! That is incorrect. The correct answer is " + correctAnswer + ".");
-        }
+
+
     }
 }
