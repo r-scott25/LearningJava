1d2ff816cfdd603af6093304fd38648ba3ce2554
r-scott25 <rachelbernal2@gmail.com>
2022-12-07 10:59:44 -0600

    foundations

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World! It's me, Rachel!");
+        int studentAge = 15;
+        double studentGPA = 3.4;
+        boolean hasPerfectAttendance = true;
+
+        String studentFirstName = "Kayla";
+        String studentLastName = "Hammon";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA + ".");
+        System.out.println("What do you want to update it to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+
+        System.out.println(studentFirstName + " " + studentLastName + " now has a GPA of " + studentGPA + ".");
     }
 }
