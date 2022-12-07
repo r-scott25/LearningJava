c32491a1f25f01a50b9686aef1a49579f4fd6b7a
r-scott25 <rachelbernal2@gmail.com>
2022-12-07 12:18:04 -0600

    control flow

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,104 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int studentAge = 15;
-        double studentGPA = 3.4;
-        boolean hasPerfectAttendance = true;
 
-        String studentFirstName = "Kayla";
-        String studentLastName = "Hammon";
-        char studentFirstInitial = studentFirstName.charAt(0);
-        char studentLastInitial = studentLastName.charAt(0);
+        //////////////////////////**** STUDENT *****///////////////////////
+        ////////////////////  Data Types and Variables  ///////////////////
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(hasPerfectAttendance);
-        System.out.println(studentFirstName);
-        System.out.println(studentLastName);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
+//        int studentAge = 15;
+//        double studentGPA = 3.4;
+//        boolean hasPerfectAttendance = true;
+//
+//        String studentFirstName = "Kayla";
+//        String studentLastName = "Hammon";
+//        char studentFirstInitial = studentFirstName.charAt(0);
+//        char studentLastInitial = studentLastName.charAt(0);
+//
+//        System.out.println(studentAge);
+//        System.out.println(studentGPA);
+//        System.out.println(hasPerfectAttendance);
+//        System.out.println(studentFirstName);
+//        System.out.println(studentLastName);
+//        System.out.println(studentFirstInitial);
+//        System.out.println(studentLastInitial);
+//
+//        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA + ".");
+//        System.out.println("What do you want to update it to?");
+//
+//
+//        Scanner input = new Scanner(System.in);
+//        studentGPA = input.nextDouble();
+//
+//        System.out.println(studentFirstName + " " + studentLastName + " now has a GPA of " + studentGPA + ".");
 
-        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA + ".");
-        System.out.println("What do you want to update it to?");
 
+
+
+        //////////////////////////**** FORTUNE TELLER *****///////////////////////
+        //////////////////////////     If Statement        ///////////////////////
+
+//        System.out.println("Pick a number between 1 and 10");
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int inputtedNum = scanner.nextInt();
+//
+//        if (inputtedNum < 5){
+//            System.out.println("Enjoy the good luck a friend bring you.");
+//        } else {
+//            System.out.println("Your shoe selection will make you happy today.");
+//        }
+
+
+
+        //////////////////////////**** Song on Repeat *****///////////////////////
+        ///////////////////////////     While Loop        ///////////////////////
+
+//    Scanner input = new Scanner(System.in);
+//    boolean isOnRepeat = true;
+//    while(isOnRepeat) {
+//        System.out.println("Playing current song");
+//        System.out.println("Would you like to take this song off of repeat? If so, answer yes");
+//        String userInput = input.next();
+//
+//        if (userInput.equals("yes")) {
+//            isOnRepeat = false;
+//        }
+//    }
+//        System.out.println("Playing next song");
+
+
+
+
+        //////////////////////////**** Challenge *****///////////////////////
+        ///////////////////////     Multiple Choice     /////////////////////
+
+        String question = "What is my favorite color?";
+        String choiceOne = "red";
+        String choiceTwo = "green";
+        String choiceThree = "blue";
+
+        String correctAnswer = choiceTwo;
+//        Write a print statement asking the question
+        System.out.println(question);
+
+//        Write a print statement giving the answer choices
+        System.out.println("Choose one: " + choiceOne + "  " + choiceTwo + "  " + choiceThree);
+
+//        Have the user input an answer
         Scanner input = new Scanner(System.in);
-        studentGPA = input.nextDouble();
 
-        System.out.println(studentFirstName + " " + studentLastName + " now has a GPA of " + studentGPA + ".");
+//        Retrive the user's input
+        String userAnswer = input.next();
+
+//        If the user's input matches the correct answer, print a congrats message
+        if (userAnswer.toLowerCase().equals(correctAnswer)) {
+            System.out.println("Congratulations! You answered correctly!");
+
+//         If the user's input does not match the correct answer, print an "incorrect" message
+        } else {
+            System.out.println("Sorry! That is incorrect. The correct answer is " + correctAnswer + ".");
+        }
     }
 }
